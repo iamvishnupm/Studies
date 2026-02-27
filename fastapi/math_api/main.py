6615c5816c3421f7dd94b6a0115dccdec1b82173
Vishnu <i.am.vishnupm@gmail.com>
2026-02-27 20:44:23 +0530

    few simple math operations directly through api

--- fastapi/math_api/main.py
+++ fastapi/math_api/main.py
@@ -1,8 +1,23 @@
+from pydantic import BaseModel
+from typing import List
+
 from fastapi import FastAPI
 
+class Numbers(BaseModel):
+    nums : List[int]
 
-app = FastAPI()
 
-@app.get("/add/{x}/{y}")
-def add(x: int, y: int):
-    return f"{x} + {y} = { x + y }"
+api = FastAPI()
+
+
+@api.post("/add")
+def add(data: Numbers):
+
+    n = len(data.nums)
+
+    return (" + ".join( ["{}"] * n ) ).format( *data.nums ) + f" = { sum(data.nums) }"
+
+
+@api.get("/multiply/{x}/{y}")
+def multiply(x: int, y: int):
+    return f"{x} x {y} = { x * y }"
